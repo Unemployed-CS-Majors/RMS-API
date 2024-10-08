2e7700db36b0c4002016257d67db6f4637452fd9
Jakub Olszewski <j.olszewski05@gmail.com>
2024-10-08 19:02:22 +0100

    Register enpoint

--- functions/index.js
+++ functions/index.js
@@ -10,17 +10,25 @@
 const functions = require("firebase-functions");
 const logger = require("firebase-functions/logger");
 const testRouter = require('./app/routes/test.router');
+const registerRouter = require('./app/routes/register.router');
 const express = require('express')
 const admin = require('firebase-admin')
+var serviceAccount = require("./firebase-admin-secret.json");
 const app = express()
 
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
 app.use("/test", testRouter)
+app.use("/auth", registerRouter)
 
 app.use((req, res) => {
     res.status(404).json({ error: 'Endpoint not found' });
 });
+const port = 3004;
 
-  const port = 3000;
   app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
   });
