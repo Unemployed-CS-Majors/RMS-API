92a4c52b907714d12805ba7151b9c36cd78f8c5d
Jakub Olszewski <j.olszewski05@gmail.com>
2024-10-08 19:45:43 +0100

    Revert last two commits

--- functions/app/routes/register.router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const RegisterController = require('../controllers/register.controller');
-
-router.post('/register', RegisterController.register);
-
-module.exports = router;
--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,28 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
 const functions = require("firebase-functions");
-const express = require("express");
-const admin = require("firebase-admin");
-const testRouter = require("./app/routes/test.router");
-const registerRouter = require("./app/routes/register.router");
-const app = express();
+const logger = require("firebase-functions/logger");
+const testRouter = require('./app/routes/test.router');
+const express = require('express')
+const admin = require('firebase-admin')
+const app = express()
 
-// Determine if running in emulator or production
-const isEmulator = process.env.FUNCTIONS_EMULATOR === "true";
-
-if (isEmulator) {
-  // Use emulator configuration
-  admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-  });
-} else {
-  // Use production configuration
-  try {
-    admin.initializeApp();
-  } catch (error) {
-    console.error("Error parsing service account key:", error);
-    process.exit(1); // Exit the process with an error code
-  }
-}
-
-app.use("/test", testRouter);
-app.use("/auth", registerRouter);
+app.use("/test", testRouter)
 
 app.use((req, res) => {
-  res.status(404).json({ error: "Endpoint not found" });
+    res.status(404).json({ error: 'Endpoint not found' });
 });
 
-const port =  3000;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+  const port = 3000;
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
 
 exports.app = functions.https.onRequest(app);
