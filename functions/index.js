ea29fec781f38eb54ca6baa52dbe6965a5542b51
Jakub Olszewski <j.olszewski05@gmail.com>
2024-10-08 19:44:39 +0100

    Register enpoint

--- functions/index.js
+++ functions/index.js
@@ -1,36 +1,38 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const functions = require("firebase-functions");
-const logger = require("firebase-functions/logger");
-const testRouter = require('./app/routes/test.router');
-const registerRouter = require('./app/routes/register.router');
-const express = require('express')
-const admin = require('firebase-admin')
-var serviceAccount = require("./firebase-admin-secret.json");
-const app = express()
+const express = require("express");
+const admin = require("firebase-admin");
+const testRouter = require("./app/routes/test.router");
+const registerRouter = require("./app/routes/register.router");
+const app = express();
 
+// Determine if running in emulator or production
+const isEmulator = process.env.FUNCTIONS_EMULATOR === "true";
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (isEmulator) {
+  // Use emulator configuration
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+  });
+} else {
+  // Use production configuration
+  try {
+    admin.initializeApp();
+  } catch (error) {
+    console.error("Error parsing service account key:", error);
+    process.exit(1); // Exit the process with an error code
+  }
+}
 
-app.use("/test", testRouter)
-app.use("/auth", registerRouter)
+app.use("/test", testRouter);
+app.use("/auth", registerRouter);
 
 app.use((req, res) => {
-    res.status(404).json({ error: 'Endpoint not found' });
+  res.status(404).json({ error: "Endpoint not found" });
 });
-const port = 3004;
 
-  app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-  });
+const port =  3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
 
 exports.app = functions.https.onRequest(app);
