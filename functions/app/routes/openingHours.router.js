061591513c76df50aace67bbdcf2c1e18a78ec00
Jakub Olszewski <j.olszewski05@gmail.com>
2024-11-07 22:05:12 +0000

    docs update

--- functions/app/routes/openingHours.router.js
+++ functions/app/routes/openingHours.router.js
@@ -6,6 +6,34 @@ const OpeningHoursController = require("../controllers/openingHours.controller")
 const { verifyIdToken } = require("../middlewares/auth.middleware");
 const { isOwner } = require("../middlewares/privilages.middleware");
 
+/**
+ * @swagger
+ * /opening-hours/{id}:
+ *   get:
+ *     summary: Retrieve opening hours by ID
+ *     description: Retrieve the opening hours for a specific ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the opening hours to retrieve.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: A successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                 openingHours:
+ *                   type: string
+ *       404:
+ *         description: Opening hours not found
+ */
 router.get("/:id", OpeningHoursController.getOpeningHoursById);
 router.get("/", OpeningHoursController.getAllOpeningHours);
 router.post(
